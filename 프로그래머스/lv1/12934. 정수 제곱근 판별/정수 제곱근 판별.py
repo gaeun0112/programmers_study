c7c683d4b90e5944a3d3c4ce16a64ad26d77b367
Gaeun Seo <85860941+gaeun0112@users.noreply.github.com>
2022-12-26 20:41:16 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
@@ -2,7 +2,6 @@ def solution(n):
     answer = 0
     for i in range(1, n+1):
         if (i * i) == n:
-            #answer = (i+1)**2
             return (i+1)**2
         else:
             answer=-1
